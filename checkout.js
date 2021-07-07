257875ab98e916f8d63592d5f77330cc381ada27
Abhijeet Kumar Gupta <84043354+AbhijeetKumarGupta@users.noreply.github.com>
2021-07-07 09:13:52 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -11,63 +11,63 @@ $(document).ready(function () {
   var totalItemsCount = 0;
   var totalItemsAmount = 0;
   var orderItemArr = [];
+  $.get(
+    "https://5d76bf96515d1a0014085cf9.mockapi.io/product",
+    function (response) {
+      for (var i = 0; i < idQuantityMapKeyList.list.length; i++) {
+        var prodId = idQuantityMapKeyList.list[i];
+        console.log(prodId);
+        for (var j = 0; j < response.length; j++) {
+          if (response[j].id == prodId) {
+            $("#leftDiv").append(
+              $("<div>")
+                .attr("class", "addedProdCard")
+                .append(
+                  $("<div>")
+                    .attr("class", "prodAddedImg")
+                    .append($("<img>").attr("src", response[j].preview)),
+                  $("<div>")
+                    .attr("class", "prodAddedDetails")
+                    .append(
+                      $("<h4>").html(response[j].name),
+                      $("<p>").html("x" + idQuantityMap[response[j].id]),
+                      $("<p>").html("Amount: Rs " + response[j].price)
+                    )
+                )
+            );
 
-  for (var i = 0; i < idQuantityMapKeyList.list.length; i++) {
-    var prodId = idQuantityMapKeyList.list[i];
-    $.get(
-      "https://5d76bf96515d1a0014085cf9.mockapi.io/product/" + prodId,
-      function (response) {
-        totalItemsCount += idQuantityMap[response.id];
-        totalItemsAmount += idQuantityMap[response.id] * response.price;
-        $("#cart-count").html(totalItemsCount);
-        $("#item-count").html(idQuantityMapKeyList.list.length);
-        $("#total-amount").html(totalItemsAmount);
+            /////////////////////////////////
 
-        var prodObj = {
-          id: response.id,
-          brand: response.brand,
-          name: response.name,
-          price: response.price,
-          preview: response.preview,
-          isAccessory: response.isAccessory,
-        };
-        for (var j = 0; j < idQuantityMap[response.id]; j++) {
-          orderItemArr.push(prodObj);
+            totalItemsCount += idQuantityMap[response[j].id];
+            totalItemsAmount +=
+              idQuantityMap[response[j].id] * response[j].price;
+            $("#cart-count").html(totalItemsCount);
+            $("#item-count").html(idQuantityMapKeyList.list.length);
+            $("#total-amount").html(totalItemsAmount);
+
+            /////////////////////////////////
+
+            var prodObj = {
+              id: response[j].id,
+              brand: response[j].brand,
+              name: response[j].name,
+              price: response[j].price,
+              preview: response[j].preview,
+              isAccessory: response[j].isAccessory,
+            };
+
+            for (var k = 0; k < idQuantityMap[response[j].id]; k++) {
+              orderItemArr.push(prodObj);
+            }
+          }
         }
       }
-    );
-  }
-  console.log(orderItemArr);
-  console.log(totalItemsAmount);
-  for (var i = 0; i < idQuantityMapKeyList.list.length; i++) {
-    var prodId = idQuantityMapKeyList.list[i];
-    $.get(
-      "https://5d76bf96515d1a0014085cf9.mockapi.io/product/" + prodId,
-      function (response) {
-        $("#leftDiv").append(
-          $("<div>")
-            .attr("class", "addedProdCard")
-            .append(
-              $("<div>")
-                .attr("class", "prodAddedImg")
-                .append($("<img>").attr("src", response.preview)),
-              $("<div>")
-                .attr("class", "prodAddedDetails")
-                .append(
-                  $("<h4>").html(response.name),
-                  $("<p>").html("x" + idQuantityMap[response.id]),
-                  $("<p>").html("Amount: Rs " + response.price)
-                )
-            )
-        );
-      }
-    );
-  }
+    }
+  );
 
   $("#button").click(function () {
     var url = "orderPlaced.html";
     var totalAmount = 0;
-
     for (var i = 0; i < orderItemArr.length; i++) {
       totalAmount += orderItemArr[i].price;
     }
